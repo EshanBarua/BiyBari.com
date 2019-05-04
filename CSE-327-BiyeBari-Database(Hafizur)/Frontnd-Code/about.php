c57b3dedcc8112b897e54336638329679b45735d
Hafizur Rahman <48995708+HafizurRahman111@users.noreply.github.com>
2019-05-04 15:13:49 +0600

    Add files via upload

---
+++ CSE-327-BiyeBari-Database(Hafizur)/Frontnd-Code/about.php
@@ -0,0 +1,19 @@
+<?php
+	include 'includes/header.php';
+	include 'includes/left-bar.php';
+?>
+	<div class="col-md-6" style="background-color:#34495E ;min-height:570px;">
+		<br/>
+		<br/><br/><br/><br/><br/>
+		
+        <div class="row centered-form">
+			Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    	</div>
+			
+    	
+    </div>
+	<div class="col-md-4" style="background: url('images/4.jpg') no-repeat;background-size: 100%;min-height:570px;">
+	
+	</div>
+	</div>
+<?php include 'includes/footer.php'?>			
